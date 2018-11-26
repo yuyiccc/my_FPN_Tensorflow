f9d0367e898e013ad554272175aa3c82ae04b13b
yujack333 <yujack333@gmail.com>
2018-11-26 20:13:16 +0800

    write and test make_anchors.py

--- FPN_code/libs/box_utils/make_anchor.py
+++ FPN_code/libs/box_utils/make_anchor.py
@@ -3,6 +3,8 @@
 # Time :2018/11/26 15:32
 
 import tensorflow as tf
+import numpy as np
+import cv2
 
 def make_anchors(base_anchor_size,
                  anchor_scales,
@@ -25,7 +27,7 @@ def make_anchors(base_anchor_size,
         # create different ratios and scales anchors
         # [num_anchors_per_location, 4]
         base_anchor = tf.constant([0, 0, base_anchor_size, base_anchor_size], dtype=tf.float32)
-        base_anchors = enum_ratios(enum_scales(base_anchor, anchor_ratios), anchor_scales)
+        base_anchors = enum_ratios(enum_scales(base_anchor, anchor_scales), anchor_ratios)
 
         _, _, hs, ws = tf.unstack(base_anchors, axis=1)
 
@@ -54,7 +56,7 @@ def enum_ratios(base_anchor, anchor_ratios, name='enum_ratios'):
     :return:
     '''
     _, _, h, w = tf.unstack(base_anchor, axis=1)
-    ratios = tf.expand_dims(anchor_ratios, axis=1)
+    ratios = tf.expand_dims(tf.sqrt(tf.cast(anchor_ratios, dtype=tf.float32)), axis=1)
     hs = tf.reshape(h*ratios, shape=[-1])
     ws = tf.reshape(w/ratios, shape=[-1])
 
@@ -64,6 +66,7 @@ def enum_ratios(base_anchor, anchor_ratios, name='enum_ratios'):
                                   tf.zeros([num_anchor_per_location]),
                                   hs, ws]))
 
+
 def enum_scales(base_anchor, anchor_scales, name='enum_scales'):
     '''
     :param base_anchor:  [y_center, x_center, h, w]
@@ -72,18 +75,43 @@ def enum_scales(base_anchor, anchor_scales, name='enum_scales'):
     :return: anchors
     '''
     with tf.variable_scope(name):
-        anchor_scales = tf.reshape(anchor_scales, [-1, 1])
+        anchor_scales = tf.reshape(tf.cast(anchor_scales, dtype=tf.float32), shape=[-1, 1])
         return base_anchor*anchor_scales
 
 
+def draw_anchor_boxes(anchors, img_h, img_w):
+    img_backgrand = np.zeros([img_h*2, img_w*2, 3], dtype=np.uint8)
+    color = (255, 0, 0)
+    cv2.rectangle(img_backgrand,
+                  pt1=(int(img_w*0.1), int(img_h*0.1)),
+                  pt2=(int(img_w*1.1), int(img_h*1.1)),
+                  color=(255, 255, 255),
+                  thickness=2)
+
+    for box in anchors:
+        y_min, x_min, y_max, x_max = box
+        cv2.rectangle(img_backgrand,
+                      pt1=(int(img_w*0.1+x_min), int(img_h*0.1+y_min)),
+                      pt2=(int(img_w*0.1+x_max), int(img_h*0.1+y_max)),
+                      color=color,
+                      thickness=1)
+    cv2.imshow('show_anchors', img_backgrand)
+    cv2.waitKey(0)
+
 if __name__=='__main__':
-    anchors = make_anchors(base_anchor_size=16,
-                           anchor_scales=[0.5, 1, 2],
+    base_anchor_size = 128
+    stride = 128
+    feature_h = 5
+    feature_w = 5
+
+    anchors = make_anchors(base_anchor_size=base_anchor_size,
+                           anchor_scales=[0.5, 0.8],
                            anchor_ratios=[0.5, 1, 2],
-                           feature_h=20,
-                           feature_w=10,
-                           stride=16,
+                           feature_h=feature_h,
+                           feature_w=feature_w,
+                           stride=stride,
                            name='try_this_part')
     with tf.Session() as sess:
         acc = sess.run(anchors)
+        draw_anchor_boxes(acc, feature_h*stride, feature_w*stride)
         print(acc)
